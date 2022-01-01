376823ce6333cab7c550229af22de2b1443c30e8
Michael Gattozzi <self@mgattozzi.dev>
2022-01-01 13:31:33 -0500

    Update neovim configuration

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,4 @@
---Gfp-----------------
+-------------------
 -- Local Imports --
 -------------------
 local fs = require('fs')
@@ -45,14 +45,11 @@ local packer = require('packer')
 packer.startup(function()
     use {'wbthomason/packer.nvim', opt = true}
     -- Packages
-    use 'ElmCast/elm-vim'
-    use 'LnL7/vim-nix'
     use 'airblade/vim-gitgutter'
     use 'bronson/vim-trailing-whitespace'
     use 'cespare/vim-toml'
     use 'chriskempson/base16-vim'
     use 'editorconfig/editorconfig-vim'
-    use 'elixir-editors/vim-elixir'
     use {
       'junegunn/fzf.vim',
       requires = {{'junegunn/fzf', hook='./install --all' }}
@@ -62,143 +59,17 @@ packer.startup(function()
     use 'tbastos/vim-lua'
     use 'tpope/vim-commentary'
     use 'tpope/vim-fugitive'
-    use 'vim-airline/vim-airline'
-    use 'vim-airline/vim-airline-themes'
+    use 'nvim-lualine/lualine.nvim'
     use 'neovim/nvim-lspconfig'
+    use 'simrat39/rust-tools.nvim'
     use 'kyazdani42/nvim-web-devicons'
-    use {'pwntester/octo.nvim', config=function()
-      require"octo".setup({
-        default_remote = {"origin", "upstream"}; -- order to try remotes
-        reaction_viewer_hint_icon = "";         -- marker for user reactions
-        user_icon = " ";                        -- user icon
-        timeline_marker = "";                   -- timeline marker
-        timeline_indent = "2";                   -- timeline indentation
-        right_bubble_delimiter = "";            -- Bubble delimiter
-        left_bubble_delimiter = "";             -- Bubble delimiter
-        github_hostname = "";                    -- GitHub Enterprise host
-        snippet_context_lines = 4;               -- number or lines around commented lines
-        file_panel = {
-          size = 10,                             -- changed files panel rows
-          use_icons = true                       -- use web-devicons in file panel
-        },
-        mappings = {
-          issue = {
-            close_issue = "<space>ic",           -- close issue
-            reopen_issue = "<space>io",          -- reopen issue
-            list_issues = "<space>il",           -- list open issues on same repo
-            reload = "<C-r>",                    -- reload issue
-            open_in_browser = "<C-b>",           -- open issue in browser
-            copy_url = "<C-y>",                  -- copy url to system clipboard
-            add_assignee = "<space>aa",          -- add assignee
-            remove_assignee = "<space>ad",       -- remove assignee
-            create_label = "<space>lc",          -- create label
-            add_label = "<space>la",             -- add label
-            remove_label = "<space>ld",          -- remove label
-            goto_issue = "<space>gi",            -- navigate to a local repo issue
-            add_comment = "<space>ca",           -- add comment
-            delete_comment = "<space>cd",        -- delete comment
-            next_comment = "]c",                 -- go to next comment
-            prev_comment = "[c",                 -- go to previous comment
-            react_hooray = "<space>rp",          -- add/remove 🎉 reaction
-            react_heart = "<space>rh",           -- add/remove ❤️ reaction
-            react_eyes = "<space>re",            -- add/remove 👀 reaction
-            react_thumbs_up = "<space>r+",       -- add/remove 👍 reaction
-            react_thumbs_down = "<space>r-",     -- add/remove 👎 reaction
-            react_rocket = "<space>rr",          -- add/remove 🚀 reaction
-            react_laugh = "<space>rl",           -- add/remove 😄 reaction
-            react_confused = "<space>rc",        -- add/remove 😕 reaction
-          },
-        pull_request = {
-          checkout_pr = "<space>po",           -- checkout PR
-          merge_pr = "<space>pm",              -- merge PR
-          list_commits = "<space>pc",          -- list PR commits
-          list_changed_files = "<space>pf",    -- list PR changed files
-          show_pr_diff = "<space>pd",          -- show PR diff
-          add_reviewer = "<space>va",          -- add reviewer
-          remove_reviewer = "<space>vd",       -- remove reviewer request
-          close_issue = "<space>ic",           -- close PR
-          reopen_issue = "<space>io",          -- reopen PR
-          list_issues = "<space>il",           -- list open issues on same repo
-          reload = "<C-r>",                    -- reload PR
-          open_in_browser = "<C-b>",           -- open PR in browser
-          copy_url = "<C-y>",                  -- copy url to system clipboard
-          add_assignee = "<space>aa",          -- add assignee
-          remove_assignee = "<space>ad",       -- remove assignee
-          create_label = "<space>lc",          -- create label
-          add_label = "<space>la",             -- add label
-          remove_label = "<space>ld",          -- remove label
-          goto_issue = "<space>gi",            -- navigate to a local repo issue
-          add_comment = "<space>ca",           -- add comment
-          delete_comment = "<space>cd",        -- delete comment
-          next_comment = "]c",                 -- go to next comment
-          prev_comment = "[c",                 -- go to previous comment
-          react_hooray = "<space>rp",          -- add/remove 🎉 reaction
-          react_heart = "<space>rh",           -- add/remove ❤️ reaction
-          react_eyes = "<space>re",            -- add/remove 👀 reaction
-          react_thumbs_up = "<space>r+",       -- add/remove 👍 reaction
-          react_thumbs_down = "<space>r-",     -- add/remove 👎 reaction
-          react_rocket = "<space>rr",          -- add/remove 🚀 reaction
-          react_laugh = "<space>rl",           -- add/remove 😄 reaction
-          react_confused = "<space>rc",        -- add/remove 😕 reaction
-        },
-        review_thread = {
-          goto_issue = "<space>gi",            -- navigate to a local repo issue
-          add_comment = "<space>ca",           -- add comment
-          add_suggestion = "<space>sa",        -- add suggestion
-          delete_comment = "<space>cd",        -- delete comment
-          next_comment = "]c",                 -- go to next comment
-          prev_comment = "[c",                 -- go to previous comment
-          select_next_entry = "]q",            -- move to previous changed file
-          select_prev_entry = "[q",            -- move to next changed file
-          close_review_tab = "<C-c>",          -- close review tab
-          react_hooray = "<space>rp",          -- add/remove 🎉 reaction
-          react_heart = "<space>rh",           -- add/remove ❤️ reaction
-          react_eyes = "<space>re",            -- add/remove 👀 reaction
-          react_thumbs_up = "<space>r+",       -- add/remove 👍 reaction
-          react_thumbs_down = "<space>r-",     -- add/remove 👎 reaction
-          react_rocket = "<space>rr",          -- add/remove 🚀 reaction
-          react_laugh = "<space>rl",           -- add/remove 😄 reaction
-          react_confused = "<space>rc",        -- add/remove 😕 reaction
-        },
-        submit_win = {
-          approve_review = "<C-a>",            -- approve review
-          comment_review = "<C-m>",            -- comment review
-          request_changes = "<C-r>",           -- request changes review
-          close_review_tab = "<C-c>",          -- close review tab
-        },
-        review_diff = {
-          add_review_comment = "<space>ca",    -- add a new review comment
-          add_review_suggestion = "<space>sa", -- add a new review suggestion
-          focus_files = "<leader>e",           -- move focus to changed file panel
-          toggle_files = "<leader>b",          -- hide/show changed files panel
-          next_thread = "]t",                  -- move to next thread
-          prev_thread = "[t",                  -- move to previous thread
-          select_next_entry = "]q",            -- move to previous changed file
-          select_prev_entry = "[q",            -- move to next changed file
-          close_review_tab = "<C-c>",          -- close review tab
-          toggle_viewed = "<leader><space>",   -- toggle viewer viewed state
-        },
-        file_panel = {
-          next_entry = "n",                    -- move to next changed file
-          prev_entry = "e",                    -- move to previous changed file
-          select_entry = "<cr>",               -- show selected changed file diffs
-          refresh_files = "R",                 -- refresh changed files panel
-          focus_files = "<leader>e",           -- move focus to changed file panel
-          toggle_files = "<leader>b",          -- hide/show changed files panel
-          select_next_entry = "]q",            -- move to previous changed file
-          select_prev_entry = "[q",            -- move to next changed file
-          close_review_tab = "<C-c>",          -- close review tab
-          toggle_viewed = "<leader><r>",   -- toggle viewer viewed state
-        }
-      }})
-    end}
     use {
       'nvim-telescope/telescope.nvim',
-      requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
-    }
-    use {
-      'nvim-lua/completion-nvim',
-      requires = {}
+      requires = {
+        {'nvim-lua/popup.nvim'},
+        {'nvim-lua/plenary.nvim'},
+        {'nvim-telescope/telescope-ui-select.nvim' }
+      }
     }
     use {
       "folke/which-key.nvim",
@@ -207,76 +78,16 @@ packer.startup(function()
       end
     }
 end)
+
 ---------------------
 -- Package Imports --
 ---------------------
 local lspconfig = require('lspconfig')
-local completion = require('completion')
--------------------------
--- Completion Settings --
--------------------------
-execute([[
-  inoremap <expr> <Tab>   pumvisible() ? "\<C-n>" : "\<Tab>"
-  inoremap <expr> <S-Tab> pumvisible() ? "\<C-p>" : "\<S-Tab>"
-]])
+
 ------------------
 -- LSP Settings --
 ------------------
-local nvim_lsp = require('lspconfig')
-local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- LSP Mappings.
-  local opts = { noremap=true, silent=true }
-  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  buf_set_keymap('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('n', '<leader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-
-  -- Set some keybinds conditional on server capabilities
-  if client.resolved_capabilities.document_formatting then
-    buf_set_keymap("n", "<leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-  elseif client.resolved_capabilities.document_range_formatting then
-    buf_set_keymap("n", "<leader>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
-  end
-
-  -- Set autocommands conditional on server_capabilities
-  if client.resolved_capabilities.document_highlight then
-    vim.api.nvim_exec([[
-      hi LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
-      hi LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
-      hi LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
-      augroup lsp_document_highlight
-        autocmd!
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-    ]], false)
-  end
-
-  completion.on_attach()
-end
-
--- Use a loop to conveniently both setup defined servers
--- and map buffer local keybindings when the language server attaches
-local servers = { "rust_analyzer" }
-for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup { on_attach = on_attach }
-end
+require('rust-tools').setup({})
 
 ------------------
 -- Vim Settings --
@@ -288,7 +99,7 @@ o.autowrite = true
 o.backspace = 'indent,eol,start'
 o.backupcopy = 'yes'
 o.compatible = false
-o.completeopt = "menuone,noinsert,noselect"
+o.completeopt = "menu,menuone,noselect"
 o.expandtab = true
 o.fileencoding = 'UTF-8'
 o.formatoptions = 'tcq'
@@ -375,17 +186,6 @@ nnoremap('<leader>rg',':Rg<CR>')
 -- Remove trailing whitespace
 nmap('<leader>rtws',':%s/\\s\\+$//e<CR>')
 
-------------------------
--- Telescope Settings --
-------------------------
-local tb = "<cmd>lua require('telescope.builtin')."
-nmap('<leader>ff', tb..'find_files()<cr>')
-nmap('<leader>fg', tb..'live_grep()<cr>')
-nmap('<leader>fb', tb..'buffer()<cr>')
-nmap('<leader>fh', tb..'help_tags()<cr>')
-nmap('<leader>la', tb..'lsp_code_actions()<cr>')
-vmap('<leader>la', tb..'lsp_code_actions()<cr>')
-
 -------------------
 -- Rust Settings --
 -------------------
@@ -403,21 +203,26 @@ vim.cmd([[command! -bang -nargs=* Rg call fzf#vim#grep('rg --column --no-heading
 vim.cmd([[command! -bang -nargs=* Gfp call fzf#vim#gitfiles('', fzf#vim#with_preview('right'))]])
 
 --------------------------
--- Vim-Airline Settings --
+-- Lualine Settings --
 --------------------------
-local a = 'airline#extensions#'
-local gset = vim.api.nvim_set_var
-gset(a..'tabline#enabled', 1)
-gset(a..'branch#enabled', 1)
-gset(a..'branch#empty_message', '')
-gset(a..'branch#use_vcscommand', 0)
-gset(a..'branch#displayed_head_limit', 10)
-gset(a..'branch#format', 0)
-gset(a..'hunks#enabled', 1)
-gset(a..'hunks#non_zero_only', 0)
-gset(a..'hunks#hunk_symbols', "['+', '~', '-']")
-gset('airline_powerline_fonts', 1)
+require('lualine').setup()
 
--------------------
--- Octo Settings --
--------------------
+------------------------
+-- Telescope Settings --
+------------------------
+local tb = "<cmd>lua require('telescope.builtin')."
+nmap('<leader>ff', tb..'find_files()<cr>')
+nmap('<leader>fg', tb..'live_grep()<cr>')
+nmap('<leader>fb', tb..'buffer()<cr>')
+nmap('<leader>fh', tb..'help_tags()<cr>')
+nmap('<leader>la', tb .. 'lsp_code_actions(require("telescope.themes").get_cursor())<cr>')
+
+require("telescope").setup {
+  extensions = {
+    ["ui-select"] = {
+      require("telescope.themes").get_dropdown {}
+    }
+  }
+}
+require("telescope").load_extension("ui-select")
+
---
+++ nvim/plugin/packer_compiled.lua
@@ -0,0 +1,197 @@
+-- Automatically generated packer.nvim plugin loader code
+
+if vim.api.nvim_call_function('has', {'nvim-0.5'}) ~= 1 then
+  vim.api.nvim_command('echohl WarningMsg | echom "Invalid Neovim version for packer.nvim! | echohl None"')
+  return
+end
+
+vim.api.nvim_command('packadd packer.nvim')
+
+local no_errors, error_msg = pcall(function()
+
+  local time
+  local profile_info
+  local should_profile = false
+  if should_profile then
+    local hrtime = vim.loop.hrtime
+    profile_info = {}
+    time = function(chunk, start)
+      if start then
+        profile_info[chunk] = hrtime()
+      else
+        profile_info[chunk] = (hrtime() - profile_info[chunk]) / 1e6
+      end
+    end
+  else
+    time = function(chunk, start) end
+  end
+  
+local function save_profiles(threshold)
+  local sorted_times = {}
+  for chunk_name, time_taken in pairs(profile_info) do
+    sorted_times[#sorted_times + 1] = {chunk_name, time_taken}
+  end
+  table.sort(sorted_times, function(a, b) return a[2] > b[2] end)
+  local results = {}
+  for i, elem in ipairs(sorted_times) do
+    if not threshold or threshold and elem[2] > threshold then
+      results[i] = elem[1] .. ' took ' .. elem[2] .. 'ms'
+    end
+  end
+
+  _G._packer = _G._packer or {}
+  _G._packer.profile_output = results
+end
+
+time([[Luarocks path setup]], true)
+local package_path_str = "/home/michael/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/michael/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/michael/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/michael/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/michael/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+if not string.find(package.path, package_path_str, 1, true) then
+  package.path = package.path .. ';' .. package_path_str
+end
+
+if not string.find(package.cpath, install_cpath_pattern, 1, true) then
+  package.cpath = package.cpath .. ';' .. install_cpath_pattern
+end
+
+time([[Luarocks path setup]], false)
+time([[try_loadstring definition]], true)
+local function try_loadstring(s, component, name)
+  local success, result = pcall(loadstring(s), name, _G.packer_plugins[name])
+  if not success then
+    vim.schedule(function()
+      vim.api.nvim_notify('packer.nvim: Error running ' .. component .. ' for ' .. name .. ': ' .. result, vim.log.levels.ERROR, {})
+    end)
+  end
+  return result
+end
+
+time([[try_loadstring definition]], false)
+time([[Defining packer_plugins]], true)
+_G.packer_plugins = {
+  ["base16-vim"] = {
+    loaded = true,
+    path = "/home/michael/.local/share/nvim/site/pack/packer/start/base16-vim",
+    url = "https://github.com/chriskempson/base16-vim"
+  },
+  ["editorconfig-vim"] = {
+    loaded = true,
+    path = "/home/michael/.local/share/nvim/site/pack/packer/start/editorconfig-vim",
+    url = "https://github.com/editorconfig/editorconfig-vim"
+  },
+  fzf = {
+    loaded = true,
+    path = "/home/michael/.local/share/nvim/site/pack/packer/start/fzf",
+    url = "https://github.com/junegunn/fzf"
+  },
+  ["fzf.vim"] = {
+    loaded = true,
+    path = "/home/michael/.local/share/nvim/site/pack/packer/start/fzf.vim",
+    url = "https://github.com/junegunn/fzf.vim"
+  },
+  ["lualine.nvim"] = {
+    loaded = true,
+    path = "/home/michael/.local/share/nvim/site/pack/packer/start/lualine.nvim",
+    url = "https://github.com/nvim-lualine/lualine.nvim"
+  },
+  ["nvim-lspconfig"] = {
+    loaded = true,
+    path = "/home/michael/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    url = "https://github.com/neovim/nvim-lspconfig"
+  },
+  ["nvim-web-devicons"] = {
+    loaded = true,
+    path = "/home/michael/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    url = "https://github.com/kyazdani42/nvim-web-devicons"
+  },
+  ["packer.nvim"] = {
+    loaded = false,
+    needs_bufread = false,
+    path = "/home/michael/.local/share/nvim/site/pack/packer/opt/packer.nvim",
+    url = "https://github.com/wbthomason/packer.nvim"
+  },
+  ["plenary.nvim"] = {
+    loaded = true,
+    path = "/home/michael/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    url = "https://github.com/nvim-lua/plenary.nvim"
+  },
+  ["popup.nvim"] = {
+    loaded = true,
+    path = "/home/michael/.local/share/nvim/site/pack/packer/start/popup.nvim",
+    url = "https://github.com/nvim-lua/popup.nvim"
+  },
+  ["rust-tools.nvim"] = {
+    loaded = true,
+    path = "/home/michael/.local/share/nvim/site/pack/packer/start/rust-tools.nvim",
+    url = "https://github.com/simrat39/rust-tools.nvim"
+  },
+  ["rust.vim"] = {
+    loaded = true,
+    path = "/home/michael/.local/share/nvim/site/pack/packer/start/rust.vim",
+    url = "https://github.com/rust-lang/rust.vim"
+  },
+  ["telescope-ui-select.nvim"] = {
+    loaded = true,
+    path = "/home/michael/.local/share/nvim/site/pack/packer/start/telescope-ui-select.nvim",
+    url = "https://github.com/nvim-telescope/telescope-ui-select.nvim"
+  },
+  ["telescope.nvim"] = {
+    loaded = true,
+    path = "/home/michael/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    url = "https://github.com/nvim-telescope/telescope.nvim"
+  },
+  ["vim-commentary"] = {
+    loaded = true,
+    path = "/home/michael/.local/share/nvim/site/pack/packer/start/vim-commentary",
+    url = "https://github.com/tpope/vim-commentary"
+  },
+  ["vim-fugitive"] = {
+    loaded = true,
+    path = "/home/michael/.local/share/nvim/site/pack/packer/start/vim-fugitive",
+    url = "https://github.com/tpope/vim-fugitive"
+  },
+  ["vim-gitgutter"] = {
+    loaded = true,
+    path = "/home/michael/.local/share/nvim/site/pack/packer/start/vim-gitgutter",
+    url = "https://github.com/airblade/vim-gitgutter"
+  },
+  ["vim-lua"] = {
+    loaded = true,
+    path = "/home/michael/.local/share/nvim/site/pack/packer/start/vim-lua",
+    url = "https://github.com/tbastos/vim-lua"
+  },
+  ["vim-mix-format"] = {
+    loaded = true,
+    path = "/home/michael/.local/share/nvim/site/pack/packer/start/vim-mix-format",
+    url = "https://github.com/mhinz/vim-mix-format"
+  },
+  ["vim-toml"] = {
+    loaded = true,
+    path = "/home/michael/.local/share/nvim/site/pack/packer/start/vim-toml",
+    url = "https://github.com/cespare/vim-toml"
+  },
+  ["vim-trailing-whitespace"] = {
+    loaded = true,
+    path = "/home/michael/.local/share/nvim/site/pack/packer/start/vim-trailing-whitespace",
+    url = "https://github.com/bronson/vim-trailing-whitespace"
+  },
+  ["which-key.nvim"] = {
+    config = { "\27LJ\2\n;\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\14which-key\frequire\0" },
+    loaded = true,
+    path = "/home/michael/.local/share/nvim/site/pack/packer/start/which-key.nvim",
+    url = "https://github.com/folke/which-key.nvim"
+  }
+}
+
+time([[Defining packer_plugins]], false)
+-- Config for: which-key.nvim
+time([[Config for which-key.nvim]], true)
+try_loadstring("\27LJ\2\n;\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\14which-key\frequire\0", "config", "which-key.nvim")
+time([[Config for which-key.nvim]], false)
+if should_profile then save_profiles() end
+
+end)
+
+if not no_errors then
+  vim.api.nvim_command('echohl ErrorMsg | echom "Error in packer_compiled: '..error_msg..'" | echom "Please check your config for correctness" | echohl None')
+end
